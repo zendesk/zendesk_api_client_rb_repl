0ab3c301e79cc807965f87ba86aad7655b5a8954
Erik Trapin (ecoologic) <erik.trapin@zendesk.com>
2023-04-12 08:01:45 +1000

    REPL with Faraday 2

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source "https://rubygems.org"
 
-gem "zendesk_api"
+gem "zendesk_api", ">= 2"
+
 gem "pry"
 gem "pry-nav"
 gem "pry-stack_explorer"
--- main.rb
+++ main.rb
@@ -11,37 +11,59 @@ require 'zendesk_api'
 
 # You won't need to namespace every resource inside the REPL
 module ZendeskAPI
+  # Short for map_ids - A table with ID and the eval'd code you pass
+  # Example: mids(client.users.fetch!, :email, :created_at) # [[id, email, created_at]]
+  # More helpers at https://gist.github.com/ecoologic/66939216c9b2443ae643d6d114b53fcc
+  def self.mids(items, *fields)
+    items.map do |item|
+      columns = fields.map { |f| eval "item.#{f}" }
+      [item.id, *columns]
+    end
+  end
   # Client configuration
   client = ZendeskAPI::Client.new do |config|
     config.url = "https://#{ENV['ZENDESK_API_CLIENT_RB_SUBDOMAIN']}.zendesk.com/api/v2"
     config.username = ENV['ZENDESK_API_CLIENT_RB_USERNAME']
     config.password = ENV['ZENDESK_API_CLIENT_RB_PASSWORD']
   end
+  current_email = client.current_user.email
   client.config.logger.level = :debug
+  puts "\n***** #{client.config.url} - #{current_email} *****"
 
   # ** Fix CI **
   # client.topics.fetch!.map(&:destroy!)
+  # # Undeleted identities:
+  # client.users.map { |u| is = u.identities.fetch!; is.map &:value }
+  # us[5].identities[1].delete
 
   # ** Things you can call in your REPL **
   # tickets = client.tickets.fetch!
+  # client.tickets.create!(
+  #   requester: client.current_user,
+  #   organization_id: client.organizations.last.id,
+  #   subject: "Testing ticket creation",
+  #   description: "Use IDs for associations",
+  #   tags: ["tmp"],
+  # )
   #
   # article_search = client.articles.search(query: 'test')
   # puts article_search.count
   #
-  # ti = Ticket.find(client, id: 163)
-  # ti.update(status: :closed, comment: { public: false, body: 'Closing...' })
-  # ti.save
+  # ticket = Ticket.find(client, id: 163)
+  # ticket.update(status: :closed, comment: { public: false, body: 'Closing...' })
+  # ticket.save
+  #
+  # scedule = client.schedules.create(name: "East Coast", time_zone: "Eastern Time (US & Canada)") # OR...
+  # scedule = Schedule.create(client, name: "bogus schedule", time_zone: "Brisbane")
+  # scedule.update(workweek: { intervals: [{ start_time: 3420, end_time: 3900 }] })
+  # scedule.save
+  #
+  # client.connection.get(full_url)
+  # See main.rb for usage suggestions
 
-  # sc = client.schedules.create(name: "East Coast", time_zone: "Eastern Time (US & Canada)")
-  # OR sc = Schedule.create(client, name: "bogus schedule", time_zone: "Brisbane")
-  # sc.update(workweek: { intervals: [{ start_time: 3420, end_time: 3900 }] })
-  # sc.save
-
-  # ** REPL utilities **
   all_resources = ObjectSpace.each_object(Class).select { |k| k < Resource }
 
-  # ** This is your breakpoint **
-  binding.pry
+  binding.pry # ** Your breakpoint **
 
-  puts 'Bye!'
+  puts 'Bye!' # Type `exit` or `CTRL+C` to quit
 end
